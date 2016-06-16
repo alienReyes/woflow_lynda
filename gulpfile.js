556c5faaff637bcb7cda0c51c8678b4620aeffd2
Rodrigo Reyes <kid.alien@gmail.com>
2016-06-15 23:05:00 -0400

    Added coffeScript fils

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp= require('gulp'),
+    gutil=require('gulp-util')
+    coffee=require('gulp-coffee');
+
+var coffeeSources=['components/coffee/*.coffee']
+
+
+gulp.task('coffee',function(){
+ gulp.src(coffeeSources)//uses a variable
+ .pipe(coffee({bare:true})//option from  coffee script
+ .on('error',gutil.log))//catches the error
+ .pipe(gulp.dest('components/scripts/'))
+
+});
