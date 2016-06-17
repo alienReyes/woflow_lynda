1d03f975253ba979a0ebaca7eb76c4b83b3c74db
Rodrigo Reyes <kid.alien@gmail.com>
2016-06-17 10:11:04 -0400

    mixed step

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp= require('gulp'),
     gutil=require('gulp-util')
-    coffee=require('gulp-coffee');
+    coffee=require('gulp-coffee'),
+    concat=require('gulp-concat');
 
 var coffeeSources=['components/coffee/*.coffee']
 
